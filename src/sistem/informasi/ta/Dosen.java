7a79729d4294afb2ae4cdc008193445dbdf0be43
Yunita Rachma Pradiawati <yurawa99@gmail.com>
2016-03-13 12:59:32 +0700

    first init

---
+++ src/sistem/informasi/ta/Dosen.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistem.informasi.ta;
+
+/**
+ *
+ * @author yunitarp2706
+ */
+public class Dosen {
+    KelompokTA topikTA[];
+    String status;
+    String nip;
+    
+    public Dosen(String nama, String nip, String status){
+        super(nama);
+        this.nip = nip;
+        this.status = status;
+    }
+}
