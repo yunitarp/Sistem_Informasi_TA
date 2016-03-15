5c08f770d7454b127393d6dcee127e3d620529e6
Aldry <Aldry@ALDRY-PC>
2016-03-15 19:14:34 +0700

    Selesai membuat isi kelas Mahasiswa

--- src/sistem/informasi/ta/Mahasiswa.java
+++ src/sistem/informasi/ta/Mahasiswa.java
@@ -9,6 +9,46 @@ package sistem.informasi.ta;
  *
  * @author Aldry
  */
-public class Mahasiswa {
+public class Mahasiswa extends Orang implements Bimbingan {
+    private TugasAkhir tugasAkhir;
+    private String nim;
+    private String status;
     
+    /**
+     *
+     * @param nim
+     * @param status
+     * @param nama
+     */
+    public Mahasiswa(String nim,String status,String nama){
+       this.nim = nim;
+       super(nama);
+       this.status = status;
+    }
+    
+    public void createTA (String judul) {
+        tugasAkhir = new TugasAkhir(judul);
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public void displayInfoMahasiswa() {
+        System.out.println("Nama Mahasiswa : " + nama);
+        System.out.println("Nama Mahasiswa : " + nim);
+        System.out.println("Nama Mahasiswa : " + status);
+    }
 }
