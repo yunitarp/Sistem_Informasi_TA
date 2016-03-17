2064343a3c5a027194bfb514288d1083c39e1902
Bety Elysabeth <Bety Elysabeth@HP-Pavilion>
2016-03-17 12:09:04 +0700

    Penambahan kelas Orang dan pelengkapan isi kelas

--- src/sistem/informasi/ta/Orang.java
+++ src/sistem/informasi/ta/Orang.java
@@ -10,5 +10,57 @@ package sistem.informasi.ta;
  * @author Bety Elysabeth
  */
 public class Orang {
-    
+    private String nama;
+	private int umur;
+	private String noTelp;
+	private String alamat;
+	private String tanggalLahir;
+        
+        public Orang (String nama, int umur, String noTelp, String alamat, String tanggalLahir) {
+                this.nama = nama;
+                this.umur = umur;
+                this.noTelp = noTelp;
+                this.alamat = alamat;
+                this.tanggalLahir = tanggalLahir;
+        }
+
+        public void setNama (String nama){
+		this.nama = nama;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	
+	public void setUmur(int umur) {
+		this.umur = umur;
+	}
+	
+	public int getUmur() {
+		return umur;
+	}
+	
+	public void setTglLahir(String tanggalLahir) {
+		this.tanggalLahir = tanggalLahir;
+	}
+	
+	public String getTglLahir() {
+		return tanggalLahir;
+	}
+	
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	
+	public String getAlamat() {
+		return alamat;
+	}
+	
+	public void setNoTelp(String noTelp) {
+		this.noTelp = noTelp;
+	}
+	
+	public String getNoTelp() {
+		return noTelp;
+	}
 }
